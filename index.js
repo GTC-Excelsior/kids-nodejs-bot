f4c99c19b5cfd4095d1ff0f3f9bd8e44fc50a608
Ian Duke <ian@thefarmventures.com>
2017-12-05 21:23:09 -0500

    declutter as we'll integrate this with Firebase

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 "use strict";
 const PORT = 8080; // default port 8080
 const express = require("express");
-const pg = require("pg");
 const app = express();
 var router = express.Router();
 const testResults = require("./testScores.js");
-
-
-var db = require('./queries.js');
-
-
-router.get('/api/kids', db.getAllScores);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 let data = {
   results: testResults.scores,
-  scores: db.scores
 };
 
 app.get("/", (req, res) => {
